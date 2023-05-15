2edcdb6d991552f061b525f625297b2acccfcdfe
TõnisTuulik <tonis.tuulik@tktk.ee>
2023-05-15 22:54:40 +0300

    first commit

---
+++ Test14/Program.cs
@@ -0,0 +1,41 @@
+﻿Cat newCat = new Cat("Garfield", "ginger");
+Console.WriteLine($"{newCat.Name} wants a rub on his belly.");
+
+while (newCat.Bellyfull != 10)
+{
+    newCat.Sleep();
+}
+newCat.Meow();
+newCat.ShowCatData();
+class Cat
+{
+    private string _name;
+    private string _color;
+    private int _bellyfull;
+
+    public Cat(string name, string color)
+    {
+        _name = name;
+        _color = color;
+        _bellyfull = 10;
+        Console.WriteLine($"A {color} cat {name} has been created.");
+    }
+    public string Name { get { return _name;} }
+    public string Color { get { return _color;} }
+    public int Bellyfull { get { return _bellyfull;} }
+    public void Sleep()
+    {
+        _bellyfull--;
+        Console.WriteLine($"{_name} says: Z-z-Z-z-Z-z");
+    }
+    public void Meow()
+    {
+        Console.WriteLine($"{_name} says: Meow!");
+    }
+    public void ShowCatData()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Color: {_color}");
+        Console.WriteLine($"Level of hungriness: {_bellyfull}");
+    }
+}
---
+++ Week14_OOP/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine("Name your dog:");
+string myDogName = Console.ReadLine();
+
+Dog myDog = new Dog(myDogName);
+Dog neighboursDog = new Dog("Good girl");
+
+
+Console.WriteLine($"My dogs name is {myDog.Name}");
+Console.WriteLine($"My neighbours dogs name is {neighboursDog.Name}");
+
+myDog.Rename("Bad girl");
+while (myDog.LevelOfHapiness !=5)
+{ 
+    myDog.Bark();
+}
+
+myDog.Tailwag();
+
+
+class Dog
+{
+    private string _name;
+    private int _levelOfHapiness;
+    //constructor
+    public Dog(string name)
+    {
+        _name = name;
+        _levelOfHapiness = 0;
+    }
+
+    public string Name
+    {
+        get { return _name; }
+    }
+    public int LevelOfHapiness
+    {
+        get { return _levelOfHapiness; }
+    }
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has been renamed to {newName}");
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("Woof-Woof!");
+        _levelOfHapiness++;
+    }
+
+    public void Tailwag()
+    {
+        Console.WriteLine("Wiggle-wiggle!");
+    }
+}
+ 
+
+
